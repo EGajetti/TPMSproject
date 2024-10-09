b9aa72bdc07333a7e01b14f0f2fdd8dbc0fe302b
EGajetti <eleonora.gajetti@polito.it>
2024-10-09 12:34:35 +0200

    Incert num cells in config file

--- mainTPMS.cpp
+++ mainTPMS.cpp
@@ -47,15 +47,11 @@ int main(int argc, char* argv[])
 	char TPMSname = var_value[0][0];
 	string type = var_value[1];
 
-	// int numCellX = stoi(var_value[3]);
-	// int numCellY = stoi(var_value[4]);
-	// int numCellZ = stoi(var_value[5]);
-
-	int numCellX = 1;
-	int numCellY = 1;
-	int numCellZ = 1;
-
 	float tarSize = stof(var_value[2]);
+
+	int numCellX = stoi(var_value[3]);
+	int numCellY = stoi(var_value[4]);
+	int numCellZ = stoi(var_value[5]);
 
 	// double* origin = convertOrigin(var_value[3]);
 	// double trasla = (tarSize + 1.0)/nFinal;
